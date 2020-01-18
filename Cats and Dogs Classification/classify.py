20ad068260d26a9b1c12631c83cb07d16643cf13
unknown <luukhanhtung2002@gmail.com>
2020-01-18 09:36:07 +0700

    Update cat and dog classification

--- Cats and Dogs Classification/classify.py
+++ Cats and Dogs Classification/classify.py
@@ -39,7 +39,7 @@ num_dogs_val = len(os.listdir(validation_dogs_dir))
 total_train = num_cats_tr + num_dogs_tr
 total_val = num_cats_val + num_dogs_val
 
-BATCH_SIZE = 32
+BATCH_SIZE = 100
 IMG_SHAPE  = 150 # Our training data consists of images with width of 150 pixels and height of 150 pixels
 
 # This function will plot images in the form of a grid with 1 row and 5 columns where images are placed in each column.
@@ -84,13 +84,13 @@ model = keras.models.Sequential([
     keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=(150, 150, 3)),
     keras.layers.MaxPooling2D(2, 2),
 
-    keras.layers.Conv2D(128, (3,3), activation='relu'),
+    keras.layers.Conv2D(64, (3,3), activation='relu'),
     keras.layers.MaxPooling2D(2,2),
 
     keras.layers.Conv2D(128, (3,3), activation='relu'),
     keras.layers.MaxPooling2D(2,2),
 
-    keras.layers.Conv2D(32, (2,2), activation='relu'),
+    keras.layers.Conv2D(128, (3,3), activation='relu'),
     keras.layers.MaxPooling2D(2,2),
 
     keras.layers.Dropout(0.5),
